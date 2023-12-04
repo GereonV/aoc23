047fc74a81edf5eb24ac8d690cbeeb13730b5e4a
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-12-05 00:52:05 +0100

    Clean up

--- day4/Main.hs
+++ day4/Main.hs
@@ -1,4 +1,4 @@
-import Data.Bifunctor (first, second)
+import Data.Bifunctor (bimap, first, second, Bifunctor)
 import Data.Set (intersection, size)
 import qualified Data.Set as Set
 
@@ -6,16 +6,18 @@ data Card = Card { winning :: [Int], actual :: [Int] }
   deriving Show
 
 splitAtFirst :: Eq a => a -> [a] -> ([a], [a])
-splitAtFirst x (h:t) = if x == h then ([], t) else first (h:) $ splitAtFirst x t
+splitAtFirst x (h:t)
+    | h == x = ([], t)
+    | otherwise = first (h:) . splitAtFirst x $ t
+
+mapTup :: Bifunctor p => (a -> b) -> p a a -> p b b
+mapTup f = bimap f f
 
 parse :: String -> [Card]
-parse = map parseLn . lines
-  where
-    parseLn ln = Card (map read . fst . splits $ ln) (map read . snd . splits $ ln)
-    splits = splitAtFirst "|" . drop 2 . words
+parse = map (uncurry Card . mapTup (map read) . splitAtFirst "|" . drop 2 . words) . lines
 
 matching :: Card -> Int
-matching c = size $ intersection (Set.fromList . winning $ c) (Set.fromList . actual $ c)
+matching c = size . uncurry intersection . mapTup (\f -> Set.fromList . f $ c) $ (winning, actual)
 
 solution1 :: [Card] -> Int
 solution1 = sum . map (score . matching)
@@ -27,11 +29,7 @@ solution2 :: [Card] -> Int
 solution2 = solve . map (, 1)
   where
     solve [] = 0
-    solve ((c, a):t) = (a +) . solve $ cops ++ rest
-      where
-        m = matching c
-        cops = second (+ a) <$> take m t
-        rest =                  drop m t
+    solve ((c, a):t) = (a +) . solve . uncurry (++) . first (map $ second (+ a)) . splitAt (matching c) $ t
 
 main :: IO ()
 main = interact $ unlines . zipWith (show .) [solution1, solution2] . repeat . parse
