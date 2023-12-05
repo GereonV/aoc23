c0302d07878adfd1df3f98b302f61af1c0992ded
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-12-05 22:50:28 +0100

    Clean up

--- day5/Main.hs
+++ day5/Main.hs
@@ -1,6 +1,6 @@
 import Data.Bifunctor (bimap, first, second)
 import qualified Data.List as List
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Maybe (fromMaybe, listToMaybe)
 
 -- source, destination, amount
 newtype IMap = IMap [(Int, Int, Int)]
@@ -17,20 +17,18 @@ splitWhen :: (a -> Bool) -> [a] -> [[a]]
 splitWhen _ [] = []
 splitWhen p (h:t)
     | p h = []:rest
-    | null rest = [[h]]
-    | otherwise = uncurry (:) . first ((h:) . head) . splitAt 1 $ rest
+    | otherwise = uncurry (:) . first ((h:) . fromMaybe [] . listToMaybe) . splitAt 1 $ rest
   where rest = splitWhen p t
 
-mapVal :: IMap -> Int -> Int
-mapVal (IMap m) k = maybe k fst $ List.uncons . mapMaybe maybeMap $ m
-  where
-    maybeMap (s, d, n)
-        | s <= k && k < s + n = Just $ k - s + d
-        | otherwise = Nothing
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs (h:(h2:t)) = (h, h2):pairs t
 
--- get output range from start of input range
-range :: IMap -> (Int, Int) -> (Int, Int)
-range (IMap m) (i, l) = second (min l) . maybe (i, unmapped) (\(s, d, n) -> (d + i - s, n - i + s)) $ List.find (\(s, _, n) -> s <= i && i < s + n) m
+mapVal :: IMap -> Int -> Int
+mapVal (IMap m) k = fromMaybe k $ listToMaybe [k - s + d | (s, d, n) <- m, s <= k && k < s + n]
+
+mapStart :: IMap -> (Int, Int) -> (Int, Int)
+mapStart (IMap m) (i, l) = second (min l) . fromMaybe (i, unmapped) $ listToMaybe [(d + i - s, n - i + s) | (s, d, n) <- m, s <= i && i < s + n]
   where
     unmapped = case filter (i <) . map (\(s, _, _) -> s) $ m of
         [] -> l
@@ -38,24 +36,17 @@ range (IMap m) (i, l) = second (min l) . maybe (i, unmapped) (\(s, d, n) -> (d +
 
 ranges :: IMap -> (Int, Int) -> [(Int, Int)]
 ranges _ (_, 0) = []
-ranges m (i, l) = d:ranges m (i + snd d, l - snd d)
-  where d = range m (i, l)
-
-reachable :: IMap -> [(Int, Int)] -> [(Int, Int)]
-reachable m = concatMap $ ranges m
-
-pairs :: [a] -> [(a, a)]
-pairs [] = []
-pairs (h:(h2:t)) = (h, h2):pairs t
+ranges m (i, l) = x:ranges m (i + snd x, l - snd x)
+  where x = mapStart m (i, l)
 
 parse :: String -> Input
 parse = uncurry Input . bimap (map read . tail . words . head . head) (map $ imap . drop 1) . splitAt 1 . splitWhen null . lines
 
 solution1 :: Input -> Int
-solution1 (Input s m) = minimum . map (($ m) . foldl (flip mapVal)) $ s
+solution1 (Input s m) = minimum . foldl (flip $ map . mapVal) s $ m
 
 solution2 :: Input -> Int
-solution2 (Input s m) = minimum . map fst . foldl (flip reachable) (pairs s) $ m
+solution2 (Input s m) = minimum . map fst . foldl (flip $ concatMap . ranges) (pairs s) $ m
 
 main :: IO ()
 main = interact $ unlines . zipWith (show .) [solution1, solution2] . repeat . parse
